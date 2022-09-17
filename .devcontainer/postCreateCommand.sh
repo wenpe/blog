3a36f59afbe5d6a4d96ebf9622a648290d3ee4cf
wenpe <wenpe2567@gmail.com>
2022-09-17 05:41:25 +0000

    [add] add .devcontainer settings

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,8 @@
+add-apt-repository ppa:git-core/ppa
+apt update -y
+apt upgrade -y
+apt install git
+yarn global add gatsby-cli
+yarn install
+git config --global user.email "wenpe2567@gmail.com"
+git config --global user.name "wenpe"
