e87fade9009a6c88f605b2806d962f0d3c112edc
wenpe <wenpe2567@gmail.com>
2022-09-18 06:43:43 +0000

    [add] add mui and emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,20 @@ import Link from 'next/link';
 import type { InferGetStaticPropsType, NextPage } from 'next';
 import { client } from 'libs/client';
 import type { Blog, Tag } from 'types/blog';
+import { styled } from '@mui/system';
+import { css } from '@mui/styled-engine';
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const RedColor = css({
+  color: 'red',
+});
+
+const Welcome = styled('span')({
+  color: 'lightblue',
+  backgroundColor: 'blue',
+  padding: 8,
+  borderRadius: 4,
+});
 
 export const getStaticProps = async () => {
   const blog = await client.get({ endpoint: 'blog' });
@@ -23,6 +37,19 @@ type Props = {
 const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ blogs, tags }: Props) => {
   return (
     <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            App Bar
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <h1>
+        <Welcome>Welcome to</Welcome> <a href='https://nextjs.org'>Next.js!</a>
+      </h1>
+      <p>
+        <span css={RedColor}>Get started by editing</span> <code>pages/index.js</code>
+      </p>
       <ul>
         {blogs.map((blog) => (
           <li key={blog.id}>
