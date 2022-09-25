4f1755b1b685b2bb587be5652f022b536befbfa4
wenpe <wenpe2567@gmail.com>
2022-09-25 04:21:23 +0000

    [add] add ogp images

--- src/nextSeoConfig.ts
+++ src/nextSeoConfig.ts
@@ -20,6 +20,7 @@ const nextSeoConfig = {
     title: 'Home',
     description: 'エンジニアのwenpeです。ITのことや、それ以外のことなど、適当に発信しています。',
     site_name: 'Wenpe Playground',
+    image: "https://images.microcms-assets.io/assets/885d2f8a9001437884ab24d84c00b5d7/2ba5bc0d3d54481988441affeceef41a/codehighlihgt.png",
     images: [
       {
       　url: "https://images.microcms-assets.io/assets/885d2f8a9001437884ab24d84c00b5d7/2ba5bc0d3d54481988441affeceef41a/codehighlihgt.png",
