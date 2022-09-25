f55da8380c8c6a4a1fa180fa0f7241c123110a60
wenpe <wenpe2567@gmail.com>
2022-09-25 04:25:56 +0000

    [add] add ogp images

--- src/nextSeoConfig.ts
+++ src/nextSeoConfig.ts
@@ -31,6 +31,7 @@ const nextSeoConfig = {
     handle: '@wenpe2567',
     site: '@wenpe2567',
     cardType: 'summary_large_image',
+    image: "https://images.microcms-assets.io/assets/885d2f8a9001437884ab24d84c00b5d7/2ba5bc0d3d54481988441affeceef41a/codehighlihgt.png",
   },
 };
 
